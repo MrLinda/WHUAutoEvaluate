6bb6430d11fb3bd12f052f60e50723dbb68e4e5a
mrlinda <1146544859@qq.com>
2023-05-29 16:41:12 +0800

    加入新功能：可以单独修改某门课程的评分（目前是10、40、60、80、100分五档）

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ import os
 JSESSIONID = input('请输入JESSIONID：')
 insert_cookie = input('请输入insert_cookie：')
 iPlanetDirectoryPro = input('请输入iPlanetDirectoryPro：')
-delay_time = int(input('请输入完成一门课评价后的延时（单位：秒）：') or '10')
 # score = float(input('请输入每门课程评价的总分（满分100分）：') or '100.0')
 score = 100.0
 data = {}
@@ -57,7 +56,7 @@ host = 'https://ugsqs.whu.edu.cn/getXspjrwfa'
 receive = requests.post(host, headers=headers, data=data)
 info = json.loads(receive.text)['info']
 for task in info:
-    host = 'https://ugsqs.whu.edu.cn/getStudentPjPf/55/2302000/SCHOOL_ADMIN'
+    host = 'https://ugsqs.whu.edu.cn/getStudentPjPf/' + str(task['ID']) + '/' + str(task['ORGCODE']) + '/SCHOOL_ADMIN'
     headers = {
         'Referer': 'https://ugsqs.whu.edu.cn/new/student/rank/evaluate2.jsp',
         'cookie': cookie
@@ -75,22 +74,88 @@ for task in info:
     }
     receive = requests.post(host, headers=headers, data=data)
     info = json.loads(receive.text)['aaData']
-    for course in info:
-        print('课程名称：' + course['KCMC'] + '\t教师：' + course['XM'])
-        if course['PJJGID'] is None:
-            # print(course['KCMC'])
+    order = int(input('请输入操作类型（0:自动评分，1：手动修改分数（建议在自动评分后操作））：'))
+    if order == 0:
+        delay_time = int(input('请输入完成一门课评价后的延时（单位：秒）：') or '10')
+        for course in info:
+            print('课程名称：' + course['KCMC'] + '\t教师：' + course['XM'])
+            if course['PJJGID'] is None:
+                # print(course['KCMC'])
 
-            # str(task['ZBTX'])
+                # str(task['ZBTX'])
+                host = 'https://ugsqs.whu.edu.cn/getTxId'
+                data = {
+                    'kclx': course['KCLX'],
+                    'zbtx': task['ZBTX']
+                }
+                receive = requests.post(host, headers=headers, data=data)
+                # print(data)
+                # print(receive.text)
+                id = json.loads(receive.text)['info'][0]['ID']
+                # print(id)
+                host = 'https://ugsqs.whu.edu.cn/tixizhibiaolist'
+                data = {
+                    'tiid': id
+                }
+                receive = requests.post(host, headers=headers, data=data)
+                index = json.loads(receive.text)['info']['zbList']
+                dxid = []
+                dxvalue = []
+                sfjft = []
+                wdid = []
+                wdvalue = []
+                for i in index:
+                    sfjft.append(('sfjft', i['sfjft']))
+                    if i['zbtx'] == '问答题':
+                        wdid.append(('wdid', i['id']))
+                        wdvalue.append(('wdvalue', '无'))
+                    elif i['zbtx'] == '限制性问答题':
+                        wdid.append(('wdid', i['id']))
+                        wdvalue.append(('wdvalue', 0))
+                    elif i['zbtx'] == '单选题':
+                        dxid.append(('dxid', i['id']))
+                        dxvalue.append(('dxvalue', i['zbfz']))
+                        # i['id']
+                        # i['xxList']['zbxxfz']
+                data = dxid + dxvalue + sfjft + wdid + wdvalue
+                data.append(('rwid', task['ID']))
+                data.append(('xqid', xqid))
+                data.append(('jsgh', course['GH']))
+                data.append(('kch', course['KCH']))
+                data.append(('bzxh', course['BZXH']))
+                data.append(('jxbdm', course['JXBDM']))
+                data.append(('xsxh', xsxh))
+                data.append(('zf', score))
+                data.append(('pjjgid', ''))
+                host = 'https://ugsqs.whu.edu.cn/createStudentPjpf'
+                # print(data)
+                re = requests.post(host, headers=headers, data=data)
+                # print(re.text)
+                time.sleep(delay_time)
+                print("评教完成！")
+            else:
+                print('已进行过评教，跳过！')
+                time.sleep(0.1)
+        print('自动评教完成！')
+    elif order == 1:
+        index = 0
+        for course in info:
+            print('[' + str(index) + ']课程名称：' + course['KCMC'] + '\t教师：' + course['XM'])
+            index = index + 1
+        needChangeIndex = int(input('请输入需要修改评分的序号（-1表示退出程序）：'))
+        while needChangeIndex != -1:
+            level = int(input('请输入需要评分的等级（1~5分别代表最低的选项到最高的选项）：'))
+            level = 5 - level
             host = 'https://ugsqs.whu.edu.cn/getTxId'
             data = {
-                'kclx': course['KCLX'],
+                'kclx': info[needChangeIndex]['KCLX'],
                 'zbtx': task['ZBTX']
             }
             receive = requests.post(host, headers=headers, data=data)
             # print(data)
             # print(receive.text)
             id = json.loads(receive.text)['info'][0]['ID']
-            print(id)
+            # print(id)
             host = 'https://ugsqs.whu.edu.cn/tixizhibiaolist'
             data = {
                 'tiid': id
@@ -102,6 +167,7 @@ for task in info:
             sfjft = []
             wdid = []
             wdvalue = []
+            zf = 0.0
             for i in index:
                 sfjft.append(('sfjft', i['sfjft']))
                 if i['zbtx'] == '问答题':
@@ -112,28 +178,25 @@ for task in info:
                     wdvalue.append(('wdvalue', 0))
                 elif i['zbtx'] == '单选题':
                     dxid.append(('dxid', i['id']))
-                    dxvalue.append(('dxvalue', i['zbfz']))
+                    dxvalue.append(('dxvalue', i['xxList'][level]['zbxxfz']))
+                    zf += float(i['xxList'][level]['zbxxfz'])
                     # i['id']
                     # i['xxList']['zbxxfz']
             data = dxid + dxvalue + sfjft + wdid + wdvalue
             data.append(('rwid', task['ID']))
             data.append(('xqid', xqid))
-            data.append(('jsgh', course['GH']))
-            data.append(('kch', course['KCH']))
-            data.append(('bzxh', course['BZXH']))
-            data.append(('jxbdm', course['JXBDM']))
+            data.append(('jsgh', info[needChangeIndex]['GH']))
+            data.append(('kch', info[needChangeIndex]['KCH']))
+            data.append(('bzxh', info[needChangeIndex]['BZXH']))
+            data.append(('jxbdm', info[needChangeIndex]['JXBDM']))
             data.append(('xsxh', xsxh))
-            data.append(('zf', score))
-            data.append(('pjjgjd', ''))
+            data.append(('zf', zf))
+            data.append(('pjjgid', info[needChangeIndex]['PJJGID']))
             host = 'https://ugsqs.whu.edu.cn/createStudentPjpf'
             # print(data)
             re = requests.post(host, headers=headers, data=data)
             # print(re.text)
-            time.sleep(delay_time)
-            print("评教完成！")
-        else:
-            print('已进行过评教，跳过！')
-            time.sleep(0.1)
-    print('评教完成！')
+            print("修改完成！")
+            needChangeIndex = int(input('请输入需要修改评分的序号（-1表示退出程序）：'))
 
 os.system("pause")
